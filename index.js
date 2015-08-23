0526f5e9100c0c49c321175512b2e2956d78ac29
3z <353360104@qq.com>
2015-08-23 13:00:46 -0400

    finalize version 1

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 var location = require("./lib/steam_countries.min.json");
 
 function dota2location () {
+
 }
 
-dota2location.try = function () {
-	console.log(location);
+dota2location.findTillCountry = function (a) {
+	if(location[a]){
+		return location[a];
+	} else {
+		console.log("Error finding Country.");
+		return;
+	}
+};
+
+dota2location.findTillState = function (a , b) {
+	var country = this.findTillCountry(a);
+	if(country){
+		if(country["states"][b]){
+			return country["states"][b];
+		} else {
+			console.log("Error finding State");
+			return ;
+		}
+	}
+};
+
+dota2location.findTillCity = function (a , b , c) {
+	var state = this.findTillState(a,b);
+	if(state){
+		if(state["cities"][c]){
+			return state["cities"][c];
+		} else {
+			console.log("Error finding City");
+			return ;
+		}
+	}
 };
 
 module.exports = dota2location;
